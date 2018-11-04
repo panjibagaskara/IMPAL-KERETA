fbae1545040f8712c11c6443d6b53931ccb98175
panjibagaskara <panjibagaskara89@gmail.com>
2018-11-04 23:44:29 +0700

    nambah pesan

--- impal/application/controllers/Cdashboard.php
+++ impal/application/controllers/Cdashboard.php
@@ -2,8 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cdashboard extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+	}
 	public function index()
 	{
 		$this->load->view('Dashboard');
+	}
+	public function pesan(){
+		$this->load->view('Pesan');
 	}
 }
--- impal/application/views/Dashboard.php
+++ impal/application/views/Dashboard.php
@@ -5,21 +5,21 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-teal.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><style>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<style>
 body {font-family: "Roboto", sans-serif}
 .w3-bar-block .w3-bar-item{padding:16px;font-weight:bold}
 </style>
 <body>
 
 <nav class="w3-sidebar w3-bar-block w3-collapse w3-animate-left w3-card" style="z-index:3;width:250px;" id="mySidebar">
-  <a class="w3-bar-item w3-button w3-border-bottom w3-large" href="#"><img src="LOGO" style="width:80%;"></a>
-  <a class="w3-bar-item w3-button w3-hide-large w3-large" href="javascript:void(0)" onclick="w3_close()">Close <i class="fa fa-remove"></i></a>
-  <a class="w3-bar-item w3-button w3-teal" href="#">Home</a>
-  <a class="w3-bar-item w3-button" href="#">Menu 1</a>
-  <a class="w3-bar-item w3-button" href="#">Menu 2</a>
-  <a class="w3-bar-item w3-button" href="#">Menu 3</a>
-  <a class="w3-bar-item w3-button" href="#">Menu 4</a>
-  <a class="w3-bar-item w3-button" href="#">Menu 5</a>
+  <a class="w3-bar-item w3-button w3-border-bottom w3-large" href="<?php echo base_url(); ?>Cdashboard"><img src="<?php echo base_url(); ?>/img/LOGO.png" style="width:100%;"></a>
+  <a class="w3-bar-item w3-button w3-hide-large w3-large" href="javascript:void(0)" onclick="w3_close()">Kembali <i class="fa fa-remove"></i></a>
+  <a class="w3-bar-item w3-button w3-teal" href="<?php echo base_url(); ?>Cdashboard">Home</a>
+  <a class="w3-bar-item w3-button" href="<?php echo base_url(); ?>Cdashboard/pesan">Pesan Tiket</a>
+  <a class="w3-bar-item w3-button" href="#">Riwayat Pemesanan</a>
+  <a class="w3-bar-item w3-button" href="#">Lihat Jadwal</a>
   <a class="w3-bar-item w3-button" href="#">LOGOUT</a>
 </nav>
 
@@ -29,32 +29,30 @@ body {font-family: "Roboto", sans-serif}
 
 <div class="w3-main" style="margin-left:250px;">
 
-<div id="myTop" class="w3-container w3-top w3-theme w3-large">
-  <p><i class="fa fa-bars w3-button w3-teal w3-hide-large w3-xlarge" onclick="w3_open()"></i>
-  <span id="myIntro" class="w3-hide">Sistem Infromasi PT.CAHKERETA</span></p>
-</div>
+    <div id="myTop" class="w3-container w3-top w3-theme w3-large">
+        <p><i class="fa fa-bars w3-button w3-teal w3-hide-large w3-xlarge" onclick="w3_open()"></i>
+        <span id="myIntro" class="w3-hide">Cah Kereta</span></p>
+    </div>
 
-<header class="w3-container w3-theme" style="padding:64px 32px">
-  <h1 class="w3-xxxlarge">Sistem Informasi PT.CAHKERETA</h1>
-  <h2>  </h2>
-</header>
+    <header class="w3-container w3-theme" style="padding:64px 32px">
+        <h1 class="w3-xxxlarge">Cah Kereta</h1>
+        <h5> Lebih dari sekedar kereta </h5>
+    </header>
 
-<div class="w3-container" style="padding:32px">
+    <div class="w3-container" style="padding:94px 32px">
 
-<h2>Layanan Pemesanan Tiket Kereta Api</h2>
+        <h2>Layanan Pemesanan Tiket Kereta Api</h2>
 
-<ul class="w3-leftbar w3-theme-border" style="list-style:none">
- <li>Melayani Booking tiket</li>
- <li>Melihat jadwal keberangkatan</li>
- <li>Mengelola riwayat pemesanan</li>
- <li>Mengelola jadwal keberangkatan</li>
- <li>Membuat laporan harian</li>
-</ul>
-</div>
+        <ul class="w3-leftbar w3-theme-border" style="list-style:none">
+            <li>Melayani Booking tiket</li>
+            <li>Melihat jadwal keberangkatan</li>
+            <li>Mengelola riwayat pemesanan</li>
+        </ul>
+    </div>
 
-<footer class="w3-container w3-theme" style="padding:32px">
-  <p>Footer information goes here</p>
-</footer>
+    <footer class="w3-container w3-theme" style="padding:32px">
+        <p>Copyright 2018 PT. Cah Kereta. All rights reserved.</p>
+    </footer>
      
 </div>
 
